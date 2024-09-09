a882cb459b6d9c0c24cb0f958d96d052e39f2057
Ben Wainwright <bwainwright28@gmail.com>
2024-09-09 20:40:55 +0100

    fix: remove debug code

--- src/types/valid-input-output-sequence.ts
+++ src/types/valid-input-output-sequence.ts
@@ -1,7 +1,4 @@
-import { Action, Block } from "@building-blocks";
-import { LegoClient } from "@core";
-
-type AddPromisesToVoids<T> = T extends void ? void | Promise<void> : T;
+import { Block } from "@building-blocks";
 
 export type GetSequenceInput<T extends ReadonlyArray<any>> =
   T extends readonly [infer First, ...any]
@@ -16,69 +13,6 @@ export type GetSequenceOutput<T extends ReadonlyArray<any>> =
       ? OutputType<Last>
       : never
     : never;
-
-interface Which {
-  entity_id?: string;
-  area_id?: string;
-}
-
-const switchLight = async (
-  client: LegoClient,
-  onOrOff: "on" | "off",
-  target: Which
-) => {
-  const action = onOrOff === "on" ? "turn_on" : "turn_off";
-
-  await client.callService("light", action, target);
-};
-
-export const turnLivingRoomLightsOn = new Action(
-  "Turn living room lights on",
-  async (client) => {
-    await switchLight(client, "on", { area_id: "living_room" });
-  }
-);
-// expands object types one level deep
-type Expand<T> = T extends infer O ? { [K in keyof O]: O[K] } : never;
-
-// expands object types recursively
-type ExpandRecursively<T> = T extends object
-  ? T extends infer O
-    ? { [K in keyof O]: ExpandRecursively<O[K]> }
-    : never
-  : T;
-
-const oneAction = new Action("This thing", (client, input: string) => {
-  console.log("something");
-  return "string";
-});
-
-const twoAction = new Action("This thing", async (client, input: string) => {
-  console.log("something");
-  return 2;
-});
-
-const threeAction = new Action("This thing", async (client, input: number) => {
-  console.log("something");
-});
-
-const fourAction = new Action("This thing", (client) => {
-  console.log("something");
-  return 2;
-});
-
-type Baz = ValidInputOutputSequence<
-  string,
-  number,
-  readonly [
-    typeof oneAction,
-    typeof twoAction,
-    typeof threeAction,
-    typeof fourAction
-  ]
->;
-
-type Foo = typeof oneAction;
 
 type InputType<T extends Block<any, any>> = Exclude<T["inputType"], undefined>;
 type OutputType<T extends Block<any, any>> = Exclude<
