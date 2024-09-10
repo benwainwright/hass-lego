ab1bbba0eb974aafa34df7e51d45c2d245d6a23c
Ben Wainwright <bwainwright28@gmail.com>
2024-09-10 15:17:01 +0100

    chore: add test coverage

---
+++ src/building-blocks/action.spec.ts
@@ -0,0 +1,66 @@
+import { EventBus, LegoClient } from "@core";
+import { Action } from "./action.ts";
+import { mock } from "vitest-mock-extended";
+import { when } from "vitest-when";
+
+describe("the action block", () => {
+  it("calls the callback when executed and passes the result out as output", async () => {
+    const callback = vi.fn();
+
+    const client = mock<LegoClient>();
+    const bus = mock<EventBus>();
+    const input = "input";
+    const output = "output";
+
+    when(callback).calledWith(client, input).thenResolve(output);
+
+    const action = new Action<string, string>({
+      name: "This is my name",
+      callback,
+    });
+
+    const result = await action.execute(client, bus, input);
+
+    expect(result).toEqual({ output, success: true });
+  });
+
+  it("returns a failure result when an error is thrown", async () => {
+    const callback = vi.fn();
+
+    const client = mock<LegoClient>();
+    const bus = mock<EventBus>();
+    const input = "input";
+
+    when(callback)
+      .calledWith(client, input)
+      .thenReject(new Error("Failed to run callback"));
+
+    const action = new Action<string, string>({
+      name: "This is my name",
+      callback,
+    });
+
+    const result = await action.execute(client, bus, input);
+
+    expect(result).toEqual({ output: undefined, success: false });
+  });
+
+  it("rethrows error when something other than an error is thrown", async () => {
+    const callback = vi.fn();
+
+    const client = mock<LegoClient>();
+    const bus = mock<EventBus>();
+    const input = "input";
+
+    when(callback).calledWith(client, input).thenReject("A string?");
+
+    const action = new Action<string, string>({
+      name: "This is my name",
+      callback,
+    });
+
+    await expect(action.execute(client, bus, input)).rejects.toThrow(
+      "A string?"
+    );
+  });
+});
---
+++ src/building-blocks/automation.spec.ts
@@ -0,0 +1,45 @@
+import { mock } from "vitest-mock-extended";
+import { Automation } from "./automation.ts";
+import { Action } from "./action.ts";
+import { when } from "vitest-when";
+import { EventBus, LegoClient } from "@core";
+
+describe("Automation", () => {
+  it("Executes all the actions and passes the outputs through to the inputs", async () => {
+    const mockClient = mock<LegoClient>();
+
+    const input = "foo";
+
+    const action1 = mock<Action<string, number>>();
+    const output1 = 2;
+
+    const action2 = mock<Action<number, string>>();
+    const output2 = "thing";
+
+    const action3 = mock<Action<string, boolean>>();
+    const output3 = false;
+
+    const bus = mock<EventBus>();
+
+    const auto = new Automation({
+      name: "Name",
+      actions: [action1, action2, action3],
+    });
+
+    when(action1.execute)
+      .calledWith(mockClient, bus, input, auto)
+      .thenResolve({ output: output1, success: true });
+
+    when(action2.execute)
+      .calledWith(mockClient, bus, output1, auto)
+      .thenResolve({ output: output2, success: true });
+
+    when(action3.execute)
+      .calledWith(mockClient, bus, output2, auto)
+      .thenResolve({ output: output3, success: true });
+
+    const result = await auto.execute(mockClient, bus, input);
+
+    expect(result).toEqual({ result: output3, success: true });
+  });
+});
