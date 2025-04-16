e30369ce0b2a37ac51c3401bc6334dcdd010c9fa
Ben Wainwright <bwainwright28@gmail.com>
2025-04-16 07:43:46 +0100

    docs: add JSDoc

--- src/client/get-connection.ts
+++ src/client/get-connection.ts
@@ -4,15 +4,18 @@ import { LegoClient } from "./lego-client.ts";
 import { EventBus } from "@core";
 import { getWebsocketServer } from "./get-websocket-server.ts";
 
+/**
+  * Initiate a working hass-blocks connection
+  */
 export const getConnection = async (
   args?: ConnectionArgs,
 ): Promise<LegoConnection> => {
   const corsOptions = args
     ? args.corsOptions
     : {
-        origin: "*",
-        methods: ["GET", "POST"],
-      };
+      origin: "*",
+      methods: ["GET", "POST"],
+    };
 
   const bus = new EventBus();
   const config = getConfig();
